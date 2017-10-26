108f9d6ceac6edb8d716a416649b9b6553b66407
aarmstrong2014 <godmox@hotmail.com>
2017-10-26 00:56:57 -0400

    refactoring

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 var roleHarvester = require('role.harvester');
-
+var gHarvesterCap = require('globals');
+var gHarvesterSuffix = require('globals');
 module.exports.loop = function () {
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         roleHarvester.run(creep);
     }
+}
+const harvesterCount = Object.keys(Game.creeps).length;
+const harvesterCap = gHarvesterCap;
+const harvesterSuffix = gHarvesterSuffix;
+console.log(harvesterCount);
+console.log(harvesterCap);
+if (harvesterCount < harvesterCap) {
+	gHarvesterSuffix += 1;
+	Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], ("Harvester" + gHarvesterSuffix));	
+	//console.log(harvesterCount);
+	//console.log(harvesterCap);
 }
