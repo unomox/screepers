efdf6a8743a2a7d9899067c6ef102219f14e6360
BuildTools <unconfigured@null.spigotmc.org>
2017-10-26 00:22:13 -0500

    IT's WORKING

---
+++ globals.js
@@ -0,0 +1,15 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('sfdg');
+ * mod.thing == 'a thing'; // true
+ */
+const gHarvesterCap = 5;
+const gHarvesterSuffix = 1;
+
+module.exports = {
+gHarvesterCap,
+gHarvesterSuffix
+};
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var roleHarvester = require('role.harvester');
-var gHarvesterCap = require('globals');
-var gHarvesterSuffix = require('globals');
+var Globals = require('globals');
+
 module.exports.loop = function () {
 
     for(var name in Game.creeps) {
@@ -9,13 +9,13 @@ module.exports.loop = function () {
     }
 }
 const harvesterCount = Object.keys(Game.creeps).length;
-const harvesterCap = gHarvesterCap;
-const harvesterSuffix = gHarvesterSuffix;
+const harvesterCap = Globals.gHarvesterCap;
+const harvesterSuffix = Globals.gHarvesterSuffix;
 console.log(harvesterCount);
 console.log(harvesterCap);
 if (harvesterCount < harvesterCap) {
-	gHarvesterSuffix += 1;
-	Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], ("Harvester" + gHarvesterSuffix));	
+	Globals.gHarvesterSuffix += 1;
+	Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], ("Harvester" + Globals.gHarvesterSuffix));	
 	//console.log(harvesterCount);
 	//console.log(harvesterCap);
 }
