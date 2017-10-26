b2cffe062e2a60ef40a85faf0b2829cdb96f3b4f
aarmstrong2014 <godmox@hotmail.com>
2017-10-26 00:19:23 -0400

    created harvester role! Yo!

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-module.exports.loop = function () {
-    var creep = Game.creeps['Harvester2'];
+var roleHarvester = require('role.harvester');
 
-    if(creep.carry.energy < creep.carryCapacity) {
-        var sources = creep.room.find(FIND_SOURCES);
-        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources[0]);
-        }
-    }
-    else {
-        if( creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
-            creep.moveTo(Game.spawns['Spawn1']);
-        }
+module.exports.loop = function () {
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        roleHarvester.run(creep);
     }
 }
