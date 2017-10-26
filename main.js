481c6bd2b2d43456386fb244592643233fa33335
BuildTools <unconfigured@null.spigotmc.org>
2017-10-26 14:48:51 -0500

    added upgrader role

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
 var Globals = require('globals');
 
 module.exports.loop = function () {
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        roleHarvester.run(creep);
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+        	roleUpgrader.run(creep);
+        }
     }
 }
 const harvesterCount = Object.keys(Game.creeps).length;
@@ -15,7 +21,12 @@ const harvesterSuffix = Math.floor(Math.random() * 999);
 //console.log(harvesterCap);
 if (harvesterCount < harvesterCap) {
 	Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], "Harvester" + harvesterSuffix);	
-	console.log(harvesterCount);
-	console.log(harvesterCap);
-	console.log(harvesterSuffix)
+}
+
+const upgraderCount = Object.keys(Game.creeps).length;
+const upgraderCap = Globals.gUpgraderCap;
+const upgraderSuffix = Math.floor(Math.random() * 999);
+
+if (harvesterCount < harvesterCap) {
+	Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], "Upgrader" + upgraderSuffix);	
 }
