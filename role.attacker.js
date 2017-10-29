c69055e28147d521a441941bd656ac3839a88b14
BuildTools <unconfigured@null.spigotmc.org>
2017-10-29 17:44:29 -0500

    modified attacker role

--- role.attacker.js
+++ role.attacker.js
@@ -1,20 +1,16 @@
 var roleAttacker = {
 		
-
     /** @param {Creep} creep **/
     run: function(creep) {
-    	
+    	var enemies = creep.room.find(Game.HOSTILE_CREEPS);
         if(enemies.length < 0) {
-        	var attacker = Game.creeps.Attacker1;
-        	var enemies = attacker.room.find(Game.HOSTILE_CREEPS);
-        	
         	if(enemies[0] == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(enemies[0], {visualizePathStyle: {stroke: '#ffff00'}});
+                    creep.moveTo(enemies[0], {visualizePathStyle: {stroke: '#FF0000'}});
                 }
             }
 
 		else {
-			attacker.attack(enemies[0]);
+			creep.attack(enemies[0]);
         }
     }
 };
